e8ade32db8cf0993907faa2beb107fbe6975f4be
adepte-myao <gr4y0tm0r@gmail.com>
2022-11-05 14:37:43 +0700

    cancel_reservation_test added

--- internal/handlers/cancel_reservation.go
+++ internal/handlers/cancel_reservation.go
@@ -6,59 +6,96 @@ import (
 	"time"
 
 	"github.com/adepte-myao/avito_internship/internal/dtos"
+	"github.com/adepte-myao/avito_internship/internal/errors"
 	"github.com/adepte-myao/avito_internship/internal/models"
 	"github.com/adepte-myao/avito_internship/internal/storage"
 	"github.com/sirupsen/logrus"
 )
 
 type CancelReservationHandler struct {
-	logger          *logrus.Logger
-	accountRepo     *storage.AccountRepository
-	reservationRepo *storage.ReservationRepository
-	txHelper        *storage.TransactionHelper
+	Logger          *logrus.Logger
+	ReservationRepo storage.ReservationRepo
+	TxHelper        storage.SQLTransactionHelper
 }
 
-func NewCancelReservationHandler(logger *logrus.Logger, store *storage.Storage) *CancelReservationHandler {
+func NewCancelReservationHandler(Logger *logrus.Logger, store *storage.Storage) *CancelReservationHandler {
 	return &CancelReservationHandler{
-		logger:          logger,
-		accountRepo:     storage.NewAccountRepository(),
-		reservationRepo: storage.NewReservationRepository(),
-		txHelper:        storage.NewTransactionHelper(store),
+		Logger:          Logger,
+		ReservationRepo: storage.NewReservationRepository(),
+		TxHelper:        storage.NewTransactionHelper(store),
 	}
 }
 
 func (handler *CancelReservationHandler) Handle(rw http.ResponseWriter, r *http.Request) {
-	handler.logger.Info("Make reservation request received")
+	handler.Logger.Info("Make reservation request received")
 
 	var data dtos.ReservationDto
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-		handler.logger.Error("cannot decode request body: ", err.Error())
-		// TODO: find out what part of body was not decoded, maybe get more user-friendly output
+		handler.Logger.Error("cannot decode request body: ", err.Error())
+
+		rw.WriteHeader(http.StatusBadRequest)
+		outErr := errors.ResponseError{
+			Reason: "invalid request body",
+		}
+		json.NewEncoder(rw).Encode(outErr)
 		return
 	}
 
-	tx, err := handler.txHelper.BeginTransaction()
+	tx, err := handler.TxHelper.BeginTransaction()
 	if err != nil {
-		// TODO
+		// Shouldn't be there
 		return
 	}
-	defer handler.txHelper.RollbackTransaction(tx)
+	defer handler.TxHelper.RollbackTransaction(tx)
 
-	reservation, err := handler.reservationRepo.GetReservation(tx, data, models.Reserved)
+	reservation, err := handler.ReservationRepo.GetReservation(tx, data, models.Reserved)
 	if err != nil {
-		// TODO
+		handler.Logger.Errorf("no reserved reservations with params: accountID: %d, serviceID: %d, orderID: %d, totalCost: %s exist",
+			data.AccountId, data.ServiceId, data.OrderId, data.TotalCost.String())
+
+		rw.WriteHeader(http.StatusBadRequest)
+		outErr := errors.ResponseError{
+			Reason: "reserved reservation with given params does not exist",
+		}
+		json.NewEncoder(rw).Encode(outErr)
+		return
+	}
+
+	reservation, err = handler.ReservationRepo.GetReservation(tx, data, models.Cancelled)
+	if err == nil {
+		handler.Logger.Errorf("already cancelled reservation with params: accountID: %d, serviceID: %d, orderID: %d, totalCost: %s",
+			data.AccountId, data.ServiceId, data.OrderId, data.TotalCost.String())
+
+		rw.WriteHeader(http.StatusBadRequest)
+		outErr := errors.ResponseError{
+			Reason: "given reservation is already cancelled",
+		}
+		json.NewEncoder(rw).Encode(outErr)
+		return
+	}
+
+	reservation, err = handler.ReservationRepo.GetReservation(tx, data, models.Accepted)
+	if err == nil {
+		handler.Logger.Errorf("accepted reservation with params: accountID: %d, serviceID: %d, orderID: %d, totalCost: %s",
+			data.AccountId, data.ServiceId, data.OrderId, data.TotalCost.String())
+
+		rw.WriteHeader(http.StatusBadRequest)
+		outErr := errors.ResponseError{
+			Reason: "given reservation was accepted",
+		}
+		json.NewEncoder(rw).Encode(outErr)
 		return
 	}
 
 	reservation.State = models.Cancelled
 	reservation.RecordTime = time.Now()
-	err = handler.reservationRepo.CreateReservation(tx, reservation)
+	err = handler.ReservationRepo.CreateReservation(tx, reservation)
 	if err != nil {
-		// TODO
+		// Shouldn't be there
 		return
 	}
 
-	handler.txHelper.CommitTransaction(tx)
+	handler.TxHelper.CommitTransaction(tx)
 
 	rw.WriteHeader(http.StatusNoContent)
 }
---
+++ internal/handlers/cancel_reservation_test.go
@@ -0,0 +1,151 @@
+package handlers_test
+
+import (
+	"bytes"
+	"database/sql"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/adepte-myao/avito_internship/internal/dtos"
+	"github.com/adepte-myao/avito_internship/internal/handlers"
+	"github.com/adepte-myao/avito_internship/internal/models"
+	"github.com/adepte-myao/avito_internship/internal/storage/mock_storage"
+	"github.com/golang/mock/gomock"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCancelReservationHandler(t *testing.T) {
+	type reservationRepoBehavior func(reservationRepo *mock_storage.MockReservationRepo, tx *sql.Tx,
+		dto dtos.ReservationDto, reservation models.Reservation)
+	type txHelperBehavior func(txHelper *mock_storage.MockSQLTransactionHelper, tx *sql.Tx)
+
+	testCases := []struct {
+		name                    string
+		inputBody               string
+		reservationRepoBehavior reservationRepoBehavior
+		txHelperBehavior        txHelperBehavior
+		expextedStatusCode      int
+		expectedResponseBody    string
+	}{
+		{
+			name:      "Success",
+			inputBody: `{"accountId":1,"serviceId":1,"orderId":1,"totalCost":"100.00"}`,
+			reservationRepoBehavior: func(reservationRepo *mock_storage.MockReservationRepo, tx *sql.Tx,
+				dto dtos.ReservationDto, reservation models.Reservation) {
+				reservationRepo.EXPECT().GetReservation(tx, gomock.AssignableToTypeOf(dto), models.Reserved).
+					Return(models.Reservation{State: models.Reserved}, nil)
+				reservationRepo.EXPECT().GetReservation(tx, gomock.AssignableToTypeOf(dto), models.Cancelled).
+					Return(models.Reservation{}, errors.New("not nil error"))
+				reservationRepo.EXPECT().GetReservation(tx, gomock.AssignableToTypeOf(dto), models.Accepted).
+					Return(models.Reservation{}, errors.New("not nil error"))
+				reservationRepo.EXPECT().CreateReservation(tx, gomock.AssignableToTypeOf(reservation)).Return(nil)
+			},
+			txHelperBehavior: func(txHelper *mock_storage.MockSQLTransactionHelper, tx *sql.Tx) {
+				txHelper.EXPECT().BeginTransaction().Return(&sql.Tx{}, nil)
+				txHelper.EXPECT().CommitTransaction(tx).Return()
+				txHelper.EXPECT().RollbackTransaction(tx).Return()
+			},
+			expextedStatusCode:   204,
+			expectedResponseBody: "",
+		},
+		{
+			name:      "Invalid request body",
+			inputBody: `{"accountId":"1","serviceId":1,"orderId":1,"totalCost":"100.00"}`,
+			reservationRepoBehavior: func(reservationRepo *mock_storage.MockReservationRepo, tx *sql.Tx,
+				dto dtos.ReservationDto, reservation models.Reservation) {
+			},
+			txHelperBehavior:     func(txHelper *mock_storage.MockSQLTransactionHelper, tx *sql.Tx) {},
+			expextedStatusCode:   400,
+			expectedResponseBody: "{\"reason\":\"invalid request body\"}\n",
+		},
+		{
+			name:      "Reservation does not exist",
+			inputBody: `{"accountId":1,"serviceId":1,"orderId":1,"totalCost":"100.00"}`,
+			reservationRepoBehavior: func(reservationRepo *mock_storage.MockReservationRepo, tx *sql.Tx,
+				dto dtos.ReservationDto, reservation models.Reservation) {
+				reservationRepo.EXPECT().GetReservation(tx, gomock.AssignableToTypeOf(dto), models.Reserved).
+					Return(models.Reservation{}, errors.New("not nil error"))
+			},
+			txHelperBehavior: func(txHelper *mock_storage.MockSQLTransactionHelper, tx *sql.Tx) {
+				txHelper.EXPECT().BeginTransaction().Return(&sql.Tx{}, nil)
+				txHelper.EXPECT().RollbackTransaction(tx).Return()
+			},
+			expextedStatusCode:   400,
+			expectedResponseBody: "{\"reason\":\"reserved reservation with given params does not exist\"}\n",
+		},
+		{
+			name:      "Reservation already accepted",
+			inputBody: `{"accountId":1,"serviceId":1,"orderId":1,"totalCost":"100.00"}`,
+			reservationRepoBehavior: func(reservationRepo *mock_storage.MockReservationRepo, tx *sql.Tx,
+				dto dtos.ReservationDto, reservation models.Reservation) {
+				reservationRepo.EXPECT().GetReservation(tx, gomock.AssignableToTypeOf(dto), models.Reserved).
+					Return(models.Reservation{}, nil)
+				reservationRepo.EXPECT().GetReservation(tx, gomock.AssignableToTypeOf(dto), models.Cancelled).
+					Return(models.Reservation{}, nil)
+			},
+			txHelperBehavior: func(txHelper *mock_storage.MockSQLTransactionHelper, tx *sql.Tx) {
+				txHelper.EXPECT().BeginTransaction().Return(&sql.Tx{}, nil)
+				txHelper.EXPECT().RollbackTransaction(tx).Return()
+			},
+			expextedStatusCode:   400,
+			expectedResponseBody: "{\"reason\":\"given reservation is already cancelled\"}\n",
+		},
+		{
+			name:      "Reservation was cancelled",
+			inputBody: `{"accountId":1,"serviceId":1,"orderId":1,"totalCost":"100.00"}`,
+			reservationRepoBehavior: func(reservationRepo *mock_storage.MockReservationRepo, tx *sql.Tx,
+				dto dtos.ReservationDto, reservation models.Reservation) {
+				reservationRepo.EXPECT().GetReservation(tx, gomock.AssignableToTypeOf(dto), models.Reserved).
+					Return(models.Reservation{}, nil)
+				reservationRepo.EXPECT().GetReservation(tx, gomock.AssignableToTypeOf(dto), models.Cancelled).
+					Return(models.Reservation{}, errors.New("not nil error"))
+				reservationRepo.EXPECT().GetReservation(tx, gomock.AssignableToTypeOf(dto), models.Accepted).
+					Return(models.Reservation{}, nil)
+			},
+			txHelperBehavior: func(txHelper *mock_storage.MockSQLTransactionHelper, tx *sql.Tx) {
+				txHelper.EXPECT().BeginTransaction().Return(&sql.Tx{}, nil)
+				txHelper.EXPECT().RollbackTransaction(tx).Return()
+			},
+			expextedStatusCode:   400,
+			expectedResponseBody: "{\"reason\":\"given reservation was accepted\"}\n",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			tx := &sql.Tx{}
+			// Dont't need specific values here because in calls we just check types
+			dto := dtos.ReservationDto{}
+			reservation := models.Reservation{}
+
+			reservationRepo := mock_storage.NewMockReservationRepo(ctrl)
+			testCase.reservationRepoBehavior(reservationRepo, tx, dto, reservation)
+
+			txHelper := mock_storage.NewMockSQLTransactionHelper(ctrl)
+			testCase.txHelperBehavior(txHelper, tx)
+
+			logger := logrus.New()
+			logger.Level = logrus.FatalLevel
+
+			handler := handlers.CancelReservationHandler{
+				Logger:          logger,
+				ReservationRepo: reservationRepo,
+				TxHelper:        txHelper,
+			}
+
+			req, err := http.NewRequest("POST", "/accept-reservation",
+				bytes.NewBufferString(testCase.inputBody))
+			assert.NoError(t, err)
+			rw := httptest.NewRecorder()
+			handler.Handle(rw, req)
+
+			assert.Equal(t, testCase.expextedStatusCode, rw.Code)
+			assert.Equal(t, testCase.expectedResponseBody, rw.Body.String())
+		})
+	}
+}
